146c8749468167612e8be511779f67c4c8e4eeb1
Ankit Karnany <ankitkarnany@gmail.com>
2017-05-19 17:02:42 +0530

    Update image directives.js
    directive for checkbox and radio buttons added

--- image directives.js
+++ image directives.js
@@ -137,3 +137,60 @@ headerApp.directive('uploadImage', ['userFactory', 'modalService', function(user
         }
     };
 }]);
+
+headerApp.directive('radioBtn', function () {
+    return {
+        restrict: 'E',
+        scope: {
+            value: '=',
+            data: '='
+        },
+        controller: function ($scope) {
+            $scope.checked = 0;
+            $scope.value = $scope.data[0];
+            $scope.checkRadio = function (index) {
+                $scope.checked = index;
+                $scope.value = $scope.data[index];
+            }
+        },
+        template: '<span style="cursor: pointer;" class="icons" ng-click="checkRadio($index)" ng-repeat="d in data"><i class="fa fa-circle-o" ng-if="checked != $index"></i><i class="fa fa-dot-circle-o" ng-if="checked == $index"></i> {{d}}</span>'
+    };
+});
+
+headerApp.directive('checkbox', function () {
+    return {
+        restrict: 'E',
+        scope: {
+            value: '=',
+            name: '@'
+        },
+        controller: function ($scope) {
+            $scope.changeValue = function () {
+                $scope.value = !$scope.value;
+            }
+        },
+        template: '<label style="cursor:pointer;" class="ng-class:{\'red\':value}" ng-click="changeValue()"><span class="icons"><span ng-if="!value" class="fa fa-square-o ng-class:{\'red\':value}"></span><span ng-if="value" class="fa fa-check-square-o ng-class:{\'red\':value}"></span></span>{{name}}</label>'
+    };
+});
+
+headerApp.directive('checkboxList', function () {
+    return {
+        restrict: 'E',
+        scope: {
+            list: '=',
+            checked: '='
+        },
+        controller: function ($scope) {
+            $scope.list.forEach(function (l, i) {
+                $scope.list[i] = {
+                    name: l,
+                    checked: false
+                };
+            });
+            $scope.check = function () {
+                console.log($scope.list);
+            }
+        },
+        template: '<label class="checkbox" ng-repeat="l in list" ng-click="check()"><checkbox value="l.checked" name="{{l.name}}"></checkbox></label>'
+    };
+});
